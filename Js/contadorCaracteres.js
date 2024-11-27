d32921f2be4ff4f1205d7d49fa2f8f7a3c36748a
PanDyyG <angel.camru@gmail.com>
2024-11-27 20:23:43 +0100

    arreglo estilos para textarea preguntas + contador caracteres - Àngel

---
+++ Js/contadorCaracteres.js
@@ -0,0 +1,5 @@
+function updateCharCount(textarea) {
+    const maxLength = textarea.getAttribute('maxlength');
+    const currentLength = textarea.value.length;
+    document.getElementById('charCount').textContent = `${currentLength}/${maxLength}`;
+} 
